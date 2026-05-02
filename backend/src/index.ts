58d298c71fa6f514c1b9b50f83bdb2e1a0a9b57e
EzeZari <103504490+EzeZari@users.noreply.github.com>
2026-05-02 15:34:42 -0300

    Subo para ver error

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,35 @@
+import 'dotenv/config'; 
 import express, { Request, Response } from 'express';
+import { PrismaClient } from '@prisma/client';
+import { Pool } from 'pg';
+import { PrismaPg } from '@prisma/adapter-pg';
 
 const app = express();
 const port = 3001;
 
+// 1. Leemos la URL de tu .env
+const connectionString = process.env.DATABASE_URL;
+
+// 2. Configuramos el adaptador oficial de Postgres que exige Prisma ahora
+const pool = new Pool({ connectionString });
+const adapter = new PrismaPg(pool);
+const prisma = new PrismaClient({ adapter });
+
+app.use(express.json());
+
 app.get('/', (req: Request, res: Response) => {
   res.send('¡Hola Ezequiel! El backend de AseguraSimple está funcionando 🚀');
+});
+
+// Ruta de prueba
+app.get('/api/asegurados', async (req: Request, res: Response) => {
+  try {
+    const asegurados = await prisma.asegurado.findMany();
+    res.json(asegurados);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Hubo un error al buscar los asegurados' });
+  }
 });
 
 app.listen(port, () => {
